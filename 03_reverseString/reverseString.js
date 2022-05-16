a1e475b3fd7aaf8df0dfb29c06a6cb359b9f62c7
gozooit <gozoo.it@protonmail.com>
2022-05-16 15:01:20 +0200

    exercise solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    let reversedString = "";
+    stringArray.reverse();
+    // return stringArray;
+    for (pos in stringArray) {
+        reversedString = reversedString + stringArray[pos];
+    };
+    return reversedString;
 };
 
 // Do not edit below this line
