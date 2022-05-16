3c35111ca5dab65ff137ce3c4266ceb9d0a4efdf
gozooit <gozoo.it@protonmail.com>
2022-05-16 14:50:30 +0200

    exercise solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, nbRepeat) {
+    if (nbRepeat < 0) {
+        return "ERROR";
+    }
+    let res = "";
+    while (nbRepeat > 0) {
+        res = res + string;
+        nbRepeat--;
+    }
+    return res;
 };
+
+console.log(repeatString("heho", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
