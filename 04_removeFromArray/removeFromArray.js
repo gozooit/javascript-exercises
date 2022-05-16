a1d5bad6cd2f4ff4b322b3a196329b7c162ec3eb
gozooit <gozoo.it@protonmail.com>
2022-05-16 17:09:07 +0200

    change arguments in the function (to continue)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === valuesToRemove) {
+            pos1 = i;
+        };
+    };
+    if (pos1) {
+        array.splice(pos1, 1);
+    }
+    return array;
 };
+
+// testArray = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(removeFromArray(testArray, "Orange"));
+
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
