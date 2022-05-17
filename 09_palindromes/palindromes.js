38f8bc9476255ed1663331ef7b0ed3727428f917
gozooit <gozoo.it@protonmail.com>
2022-05-17 20:33:17 +0200

    exercise solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    strArray = str.split("");
+    const cleanArray = clearArray(strArray);
+    let last = cleanArray.length - 1;
+    for (let i = 0; i < cleanArray.length; i++) {
+        if (!(cleanArray[i] === cleanArray[last])) {
+            return false;
+        }
+        last--;
+    }
+    return true;
 };
+
+const clearArray = function (array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i].match(/[a-z]/i)) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
