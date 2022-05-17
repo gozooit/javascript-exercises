26e7b774d83d7df8cc3917aa89622ecf942f702f
gozooit <gozoo.it@protonmail.com>
2022-05-17 11:45:03 +0200

    exercise solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const round = function(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
+const ftoc = function(t) {
+  return round(((t - 32) / 1.8), 1)
 };
 
-const ctof = function() {
-
+const ctof = function(t) {
+  return round((t * 1.8 + 32).toFixed(1), 1);
 };
 
 // Do not edit below this line
