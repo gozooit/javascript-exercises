b119d45ae9fdce08742bd711c86c09b3c4c36c79
gozooit <gozoo.it@protonmail.com>
2022-05-17 09:37:27 +0200

    exercise solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    if ((n1 < 0) || (n2 < 0) || (typeof(n1) != "number") || (typeof(n2) != "number")) {
+        return "ERROR";
+    };
+    if (n2 < n1) {
+        let tmp = n2;
+        n2 = n1;
+        n1 = tmp;
+    };
+    let sum = 0;
+    for (i = n1; i <= n2; i++) {
+        sum += i;
+    };
+    return sum;
 };
+
+console.log(sumAll(1, 5));
 
 // Do not edit below this line
 module.exports = sumAll;
